c54b0905d60548808f8f895bccd6eef54801eb56
gdj003 <gdj003@ACA88189.ipt.aol.com>
2016-03-30 16:21:05 +0800

    创建PopView工程

---
+++ PopView/PopViewTests/PopViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  PopViewTests.swift
+//  PopViewTests
+//
+//  Created by gdj003 on 16/3/30.
+//  Copyright (c) 2016年 xuliran. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PopViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
